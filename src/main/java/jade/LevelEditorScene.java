55c3566b5a8531fa2281e443b8d96e4fd56685c9
pphucc <truongdinhphuc23@gmail.com>
2023-08-28 11:23:03 +0700

    update dirty flag

--- src/main/java/jade/LevelEditorScene.java
+++ src/main/java/jade/LevelEditorScene.java
@@ -11,6 +11,8 @@ import static org.lwjgl.glfw.GLFW.*;
 
 public class LevelEditorScene extends Scene {
 
+    GameObject obj1, obj2;
+    Spritesheet sprites;
     public LevelEditorScene() {
 
     }
@@ -22,27 +24,31 @@ public class LevelEditorScene extends Scene {
 
         this.camera = new Camera(new Vector2f());
 
-        Spritesheet sprites = AssetPool.getSpritesheet("assets/images/spritesheet.png");
+        sprites = AssetPool.getSpritesheet("assets/images/spritesheet.png");
 
 
-        GameObject obj1 = new GameObject("O1", new Transform(new Vector2f(100, 100), new Vector2f(256, 256)));
+        obj1 = new GameObject("O1", new Transform(new Vector2f(100, 100), new Vector2f(256, 256)));
         obj1.addComponent(new SpriteRenderer(sprites.getSprite(0)));
         this.addGameObjectToScene(obj1);
 
-        GameObject obj2 = new GameObject("O2", new Transform(new Vector2f(400, 100), new Vector2f(256, 256)));
+        obj2 = new GameObject("O2", new Transform(new Vector2f(400, 100), new Vector2f(128, 128)));
         obj2.addComponent(new SpriteRenderer(sprites.getSprite(15)));
         this.addGameObjectToScene(obj2);
 
 
     }
 
-    private void loadResources(){
+    private void loadResources() {
         AssetPool.getShader("assets/shaders/default.glsl");
         AssetPool.addSpritesheet("assets/images/spritesheet.png",
                 new Spritesheet(AssetPool.getTexture("assets/images/spritesheet.png"),
-                        16, 16, 26,0));
+                        16, 16, 26, 0));
 
     }
+
+    private int spriteIndex = 0;
+    private float spriteFlipTime = 0.2f;
+    private float spriteFlipTimeLeft = 0.0f;
 
     @Override
     public void update(float dt) {
@@ -58,6 +64,16 @@ public class LevelEditorScene extends Scene {
         }
         if (KeyListener.isKeyPressed(GLFW_KEY_RIGHT)) {
             camera.position.x += 1;
+            spriteFlipTimeLeft -= dt;
+            if(spriteFlipTimeLeft <= 0) {
+                spriteFlipTimeLeft = spriteFlipTime;
+                spriteIndex++;
+                if(spriteIndex > 4){
+                    spriteIndex = 0;
+                }
+                obj1.getComponent(SpriteRenderer.class).setSprite(sprites.getSprite(spriteIndex));
+            }
+            obj1.transform.position.x += 1;
         }
 
         for (GameObject go : this.gameObjects) {
